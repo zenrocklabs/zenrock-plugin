922f13e5c7e3709693e849497b94ec5171c608e2
Martin <martin@zenrocklabs.io>
2025-02-14 15:40:44 -0700

    add queryUserWorkspaceAction

--- packages/plugin-zenrock/src/index.ts
+++ packages/plugin-zenrock/src/index.ts
@@ -1,6 +1,7 @@
 import { Plugin, Action, Evaluator, Provider } from "@elizaos/core";
 import { createWorkspaceAction } from "./actions/createWorkspaceAction";
 import { queryUserWorkspacesAction } from "./actions/queryUserWorkspacesAction";
+import { queryUserWorkspaceAction } from "./actions/queryUserWorkspaceAction";
 import { requestKeyAction } from "./actions/requestKeyAction";
 export const zenrockPlugin: Plugin = {
     name: "zenrock",
@@ -9,6 +10,7 @@ export const zenrockPlugin: Plugin = {
     actions: [
         createWorkspaceAction,
         queryUserWorkspacesAction,
+        queryUserWorkspaceAction,
         requestKeyAction,
     ],
     evaluators: [],
